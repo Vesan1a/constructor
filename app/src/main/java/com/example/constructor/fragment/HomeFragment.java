7eea7c4acd01a1e301fd9b40e8a57e0133924dc7
Vesan1a <114503620+Vesan1a@users.noreply.github.com>
2023-06-19 23:17:00 +0300

    Inflater

--- app/src/main/java/com/example/constructor/fragment/HomeFragment.java
+++ app/src/main/java/com/example/constructor/fragment/HomeFragment.java
@@ -9,10 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.constructor.R;
+
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 }
